ad4700fff7355c56eecc5c6d16e939bfac44e231
fmjfei <lingting@dingtalk.com>
2022-01-15 23:40:12 +0800

    测试mapper

--- src/test/java/cc/au/hkzk/test/HumEmployeeMapperTest.java
+++ src/test/java/cc/au/hkzk/test/HumEmployeeMapperTest.java
@@ -5,6 +5,7 @@ import cc.au.hkzk.mapper.HumEmployeeMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class HumEmployeeMapperTest {
 
     @Autowired
